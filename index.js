d7ff327890794d7e7e88c143e465fef1721ecca0
jenbythewood <145355065+jenbythewood@users.noreply.github.com>
2024-05-11 13:04:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,95 +1,131 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
-
-var gameCatalogue = [
-    {
-        "id": 1,
-        "title": "Game B",
-        "publisher": "Publisher ABC",
-        "developer": "Developer DEF",
-        "releaseDate": "2015-01-01",
-        "platforms": [
-            { "id": 1, "name": "Xbox" },
-            { "id": 2, "name": "Playstation" },
-            { "id": 3, "name": "PC" }
-        ],
-        "esrbRating": {
-            "id": 1,
-            "code": "E",
-            "name": "Everyone"
-        }
-    },
-    {
-        "id": 2,
-        "title": "Game C",
-        "publisher": "Publisher ABC",
-        "developer": "Developer DEF",
-        "releaseDate": "2018-01-01",
-        "platforms": [
-            { "id": 1, "name": "Xbox" },
-            { "id": 3, "name": "PC" }
-        ],
-        "esrbRating": {
-            "id": 1,
-            "code": "E",
-            "name": "Everyone"
-        }
-    },
-    {
-        "id": 3,
-        "title": "Game A",
-        "publisher": "Publisher ABC",
-        "developer": "Developer DEF",
-        "releaseDate": "2020-01-01",
-        "platforms": [
-            { "id": 1, "name": "Xbox" },
-            { "id": 2, "name": "Playstation" }
-        ],
-        "esrbRating": {
-            "id": 2,
-            "code": "M",
-            "name": "Mature"
-        }
-    }
-]
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema, assertInputType } = require("graphql");
+var express = require("express");
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    games: [Game]
+var restaurants = [
+  {
+    id: 1,
+    name: "WoodsHill ",
+    description:
+      "American cuisine, farm to table, with fresh produce every day",
+    dishes: [
+      {
+        name: "Swordfish grill",
+        price: 27,
+      },
+      {
+        name: "Roasted Broccily ",
+        price: 11,
+      },
+    ],
   },
-  type Game {
-    id: Int
-    title: String
-    publisher: String
-    developer: String
-    releaseDate: String
-    platforms: [Platform]
-    esrbRating: EsrbRating
+  {
+    id: 2,
+    name: "Fiorellas",
+    description:
+      "Italian-American home cooked food with fresh pasta and sauces",
+    dishes: [
+      {
+        name: "Flatbread",
+        price: 14,
+      },
+      {
+        name: "Carbonara",
+        price: 18,
+      },
+      {
+        name: "Spaghetti",
+        price: 19,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Karma",
+    description:
+      "Malaysian-Chinese-Japanese fusion, with great bar and bartenders",
+    dishes: [
+      {
+        name: "Dragon Roll",
+        price: 12,
+      },
+      {
+        name: "Pancake roll ",
+        price: 11,
+      },
+      {
+        name: "Cod cakes",
+        price: 13,
+      },
+    ],
+  },
+];
+var schema = buildSchema(`
+type Query{
+  restaurant(id: Int): restaurant
+  restaurants: [restaurant]
 },
-type Platform {
-    id: Int
-    name: String
-},
-type EsrbRating {
-    id: Int
-    code: String
-    name: String
+type restaurant {
+  id: Int
+  name: String
+  description: String
+  dishes:[Dish]
 }
- 
+type Dish{
+  name: String
+  price: Int
+}
+input restaurantInput{
+  name: String
+  description: String
+}
+type DeleteResponse{
+  ok: Boolean!
+}
+type Mutation{
+  setrestaurant(input: restaurantInput): restaurant
+  deleterestaurant(id: Int!): DeleteResponse
+  editrestaurant(id: Int!, name: String!): restaurant
+}
 `);
-
 // The root provides a resolver function for each API endpoint
 var root = {
-    games: () => gameCatalogue
+  restaurant: ({ id }) => {
+    return restaurants.find(restaurant => restaurant.id === id);
+  },
+  restaurants: () => {
+    return restaurants;
+  },
+  setrestaurant: ({ input }) => {
+    // create a random id for the restaurant
+    let id = Math.floor(Math.random() * 1000);
+    restaurants.push({ id, ...input });
+    return restaurants.find(restaurant => restaurant.id === id);
+  },
+  deleterestaurant: ({ id }) => {
+    const idx = restaurants.findIndex(restaurant => restaurant.id === id);
+    if (idx === -1) return { ok: false }
+    restaurants.splice(idx, 1);
+    return { ok: true }
+  },
+  editrestaurant: ({ id, ...restaurant }) => {
+    let idx = restaurants.findIndex((rest) => rest.id === id);
+    if (idx === -1) return null;
+    restaurants[idx] = { ...restaurants[idx], ...restaurant };
+    return restaurants[idx];
+  },
 };
-
 var app = express();
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+  })
+);
+var port = 5500;
+app.listen(5500, () => console.log("Running Graphql on Port:" + port));
+
+module.exports = root;
